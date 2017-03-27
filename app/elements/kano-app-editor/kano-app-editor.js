18dade4fc9483238cd82928fc39f23f34ed7f625
Viktor Dorozsmai <vikdoro@gmail.com>
2017-03-27 18:16:56 +0100

    Boolean if part-editor positioned over code-editor or window

--- app/elements/kano-app-editor/kano-app-editor.js
+++ app/elements/kano-app-editor/kano-app-editor.js
@@ -404,9 +404,15 @@ Polymer({
         this.$.partsPanel.closeDrawer();
     },
     selectedChanged (newValue) {
-        // The selection is cleared
         if (!newValue) {
             this.drawerPage = 'background-editor';
+            return;
+        }
+
+        if (newValue.fullscreenEdit) {
+            this.$['edit-part-dialog'].fitInto = window;
+        } else {
+            this.$['edit-part-dialog'].fitInto = this.$['root-view'];
         }
     },
     panelStateChanged () {
@@ -561,8 +567,6 @@ Polymer({
     },
     attached () {
         this.target = document.body;
-        this.codeEditor = this.$['root-view'];
-
         this.partEditorOpened = false;
         this.backgroundEditorOpened = false;
 
